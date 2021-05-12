bea2083ac5b5cf62f19e1bebad314314fc820178
VadimKarch <vadimkarworkmail@gmail.com>
2021-05-12 21:05:41 +0300

    Added index view method and template

--- news/views.py
+++ news/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Genre, Article
 
-# Create your views here.
+
+def index(request):
+    genries = Genre.objects.all()
+    articles = Article.objects.all().reverse()[:9]
+
+    return render(
+        request,
+        'index.html',
+        context={'genries': genries, 'articles': articles}
+    )
