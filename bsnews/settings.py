8dad0607ce234468a00f4961ac3758642e6b6f44
VadimKarch <vadimkarworkmail@gmail.com>
2021-05-13 16:49:43 +0300

    Now ready for production

--- bsnews/settings.py
+++ bsnews/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-87nw*-ghaetx9l
 #DEBUG = True
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = ['agile-ridge-73344.herokuapp.com']
+ALLOWED_HOSTS = ['agile-ridge-73344.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
