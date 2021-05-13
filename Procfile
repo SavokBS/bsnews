2c199992b081f77519e4616bdbb6aa09815e52dd
VadimKarch <vadimkarworkmail@gmail.com>
2021-05-13 13:31:36 +0300

    Ready for production?

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn bsnews.wsgi --log-file -
--- bsnews/settings.py
+++ bsnews/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -84,6 +85,11 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
